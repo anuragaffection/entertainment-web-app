920cd9d24adf6c224b3bb363e7b76d4a0408a3ca
Anurag Affection <anuragaffection07@gmail.com>
2024-07-17 11:24:43 +0530

    updated

--- frontend/src/components/FallbackComponents/FallbackMedia.jsx
+++ frontend/src/components/FallbackComponents/FallbackMedia.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { fallbackData } from './fallbackData';
 import Media from '../MediaComponents/Media'
+
 function FallbackMedia() {
+
     const mediaData = fallbackData.data;
+
+    const wrapperStyle = "flex sm:grid grid-rows-1 grid-flow-col overflow-x-scroll gap-5 lg:gap-7 scrollbar-corner-transparent scrollbar scrollbar-thumb-darkRed scrollbar-track-transparent"
 
     return (
         <>
-            <Media mediaData={mediaData} />
+            <div
+                className={wrapperStyle}
+                style={{ gridAutoColumns: "minmax(230px, auto)" }}
+            >
+                <Media mediaData={mediaData} />
+            </div>
         </>
     )
 }
